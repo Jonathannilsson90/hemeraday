d52d199a1f63d3320b8b0dbb576d4b99d48c1a41
Yulia Gutorova <j.gutorova@gmail.com>
2024-04-11 17:37:57 +0200

    fix main menu

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,11 +18,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="">
-          <button type="button" className="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br  focus:outline-none focus:ring-lime-300  shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg text-sm px-7 py-2.5 text-center me-5 mb-5 min-w-[100px]"><Link href="/"> Home </Link></button>
-          <Link href="/jonpage" > Jonpage </Link>
-          <Link href="/yuliapage" > Yuliapage </Link>
-        </div> 
+        <div className="flex items-center justify-center h-[80px] bg-cyan-200">
+          <div className="flex flex-row items-center justify-between">
+            <button type="button" className="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-600 hover:bg-gradient-to-br  focus:outline-none focus:ring-lime-300  shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg text-sm px-7 py-2.5 text-center me-5  min-w-[150px] h-[50px]"><Link href="/" className="">Home</Link></button>
+            <button type="button" className="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-600 hover:bg-gradient-to-br  focus:outline-none focus:ring-lime-300  shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg text-sm px-7 py-2.5 text-center me-5  min-w-[150px] h-[50px]"><Link href="/jonpage">Jonpage</Link></button>
+            <button type="button" className="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-600 hover:bg-gradient-to-br  focus:outline-none focus:ring-lime-300  shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg text-sm px-7 py-2.5 text-center   min-w-[150px] h-[50px]"><Link href="/yuliapage">Yuliapage</Link></button>
+          </div>
+        </div>
         {children}
       </body>
     </html>
--- app/yuliapage/page.tsx
+++ app/yuliapage/page.tsx
@@ -1,9 +1,8 @@
 const YuliaPage = () => {
   return (
-    <div className='bg-blue-200'>
+    <div className="h-screen box-border flex flex-col">
         <h1>Yulias page</h1>
     </div>
   )
 }
-
 export default YuliaPage
